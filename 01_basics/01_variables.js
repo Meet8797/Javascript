e191c519d220ac45adc66bc6c26ff7042cfcbb1e
Meet8797 <89040233+Meet8797@users.noreply.github.com>
2024-03-02 18:29:24 +0000

    Conversion of data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "meet_8797@yahoo.com"
+var accountPassword = "qwerty"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we're using node js, not browser
+
+console.log( 8 + 8) // code readablity should be high
+
+console.log("mEet")
+
+let name = "Meet"
+let age = "26"
+let isLoogedIn = false
+let state = null
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true or false
+// null => standalone value ( returns empty value)
+// undefined => variable declared but not defined
+// symbol => unique 
+
+
+// object
+
+console.log(typeof state); // object
+console.log(typeof undefined); // undefined
