ff6c96432fee977186d407bbf3a96e3299def3af
Meet8797 <89040233+Meet8797@users.noreply.github.com>
2024-03-03 00:50:14 +0000

    conversion  operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "66xya"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "66" converts to 66
@@ -15,9 +15,23 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let booleanisLoggedIn = true
-let booleanisLoggedInisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// let booleanisLoggedInisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
 
 //1 converts to true and for 0 is false
 // "" converts to false
 // "XYZ" converts to true
+
+
+// *************** Operations ****************
+
+let value = 9
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 =" meet"
+
+let str3 =(str1 + str2)
+console.log(str3)
+
