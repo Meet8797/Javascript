5a0bed294827b9461a1179fa270bdef8f38f6190
Meet8797 <89040233+Meet8797@users.noreply.github.com>
2024-03-03 15:12:54 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log("2" > 1)
+
+
+let myYutubename = "xyzYoutube"
+
+let anothername = myYutubename
+anothername = "newYoutube"
+
+console.log(anothername); 
+console.log(myYutubename); 
